fdb25dc4f9acd32ddaa332196f776ae29610613d
dyoshikawa <dyoshikawa1993@gmail.com>
2018-09-10 23:52:04 +0900

    ShinaiFormコンポーネント作成

---
+++ components/ShinaiForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <b-form-group>
+      <b-input
+        type="date"
+      />
+    </b-form-group>
+
+    <b-form-group>
+      <b-input/>
+    </b-form-group>
+
+    <b-button
+      variant="primary"
+    >
+      しません
+    </b-button>
+  </div>
+</template>
+
+<script>
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,20 @@
 <template>
-  <section class="container">
-    <div>
-      <h1 class="title">
-        shinai-list
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-      <b-button>aaa</b-button>
-    </div>
-  </section>
+  <b-container>
+    <h1 class="title">
+      しないことリスト
+    </h1>
+    <shinai-form/>
+  </b-container>
 </template>
 
 <script>
+import ShinaiForm from '~/components/ShinaiForm'
+
+export default {
+  components: {
+    ShinaiForm
+  }
+}
 </script>
 
 <style>
