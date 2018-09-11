23fe602517b74d56562f9f28a33e7c1181ee0813
dyoshikawa <dyoshikawa1993@gmail.com>
2018-09-12 00:14:45 +0900

    fix css

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,26 @@
     <nav-bar/>
     <b-container>
       <div class="box-custom box__bg_photo">
-        <h1>Not Todo List</h1>
+        <h1>More important than todo</h1>
+        <div id="login_buttons_box">
+          <div id="login_buttons">
+            <b-button
+              class="btn-google left-button"
+              variant="danger"
+            >
+              <i class="fab fa-google" />Login with Google
+            </b-button>
+
+            <br class="br-sp">
+
+            <b-button
+              class="btn-twitter"
+              variant="info"
+            >
+              <i class="fab fa-twitter" />Login with Twitter
+            </b-button>
+          </div>
+        </div>
       </div>
     </b-container>
   </div>
@@ -27,9 +46,9 @@ export default {
 <style scoped>
 .box-custom {
   height: 100vh;
-  display: flex; /* 要素をflexboxに対応させる */
-  align-items: center; /* 縦方向の中央揃え */
-  flex-direction: column; /* 子要素の並びを上から下にする（要素の改行に対応） */
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 }
 
 .box__bg_photo {
@@ -38,10 +57,26 @@ export default {
   background-position: center;
 }
 
+.br-sp {
+  display: none;
+}
+
 h1 {
   font-family: 'Gamja Flower', cursive;
   font-size: 80px;
   margin-top: 60px;
+}
+
+.left-button {
+  margin-right: 10px;
+}
+
+.btn-twitter {
+  background-color: #00aced;
+}
+
+.btn-google {
+  background-color: #dd4b39;
 }
 
 @media screen and (max-width: 768px) {
@@ -55,15 +90,35 @@ h1 {
     width: 100%;
     height: 400px;
   }
+
+  #login_buttons_box {
+    margin-top: 350px;
+    align-items: center;
+    flex-wrap: wrap;
+  }
 }
 
 @media screen and (max-width: 480px) {
   h1 {
-    font-size: 40px;
+    font-size: 30px;
   }
 
   .box-custom {
     height: 250px;
+  }
+
+  #login_buttons_box {
+    margin-top: 220px;
+    align-items: center;
+    flex-wrap: wrap;
+  }
+
+  .left-button {
+    margin-bottom: 10px;
+  }
+
+  .br-sp {
+    display: block;
   }
 }
 </style>
