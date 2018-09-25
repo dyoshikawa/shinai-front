1abc2c53a2b28b6f7ac2f25234f4876990e3bd00
crunchtimer.yoshikawa <yoshikawada@crunchtimer.jp>
2018-09-25 13:00:49 +0900

    fix config.yml

---
+++ config/config.production.js
@@ -0,0 +1,9 @@
+export const apiUrl = 'https://shinai.herokuapp.com'
+export const firebaseConfig = {
+  apiKey: "AIzaSyCYutXXDWMnJODgQwk6LRSfHnK9QuCk_FM",
+  authDomain: "shinai-11e78.firebaseapp.com",
+  databaseURL: "https://shinai-11e78.firebaseio.com",
+  projectId: "shinai-11e78",
+  storageBucket: "shinai-11e78.appspot.com",
+  messagingSenderId: "1004677563833"
+};
